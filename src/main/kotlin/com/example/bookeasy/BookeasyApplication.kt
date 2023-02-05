bdbe79e1203c735bd02aaa486b7609ce6be85bff
vitor.miranda <vitor.miranda@movile.com>
2023-02-05 17:23:46 -0300

    initial commit project

---
+++ src/main/kotlin/com/example/bookeasy/BookeasyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bookeasy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookeasyApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookeasyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bookeasy/BookeasyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bookeasy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookeasyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
